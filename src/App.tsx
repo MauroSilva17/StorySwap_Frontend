2fe167dabd0d3c926146b89794e6f4bf4b5421ac
Mauro <m.henriquesilva18@gmail.com>
2023-04-22 17:10:11 +0200

    refactor: basic strucure of StorySwap

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,13 @@ import './App.css'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Nav from './navbar/Nav'
 
 export default function App() {
     return (
         <>
             <BrowserRouter>
-                <Nav />
                 <Routes>
                     <Route path='/' element={<Home />} />
-                    <Route path='/contact' element={<Contact />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/projects' element={<Projects />} />
                 </Routes>
             </BrowserRouter>
         </>
--- src/components/random.tsx
+++ src/components/random.tsx
@@ -1,5 +1,6 @@
 export default () => {
     return (
+        //Empty component. Meant to create a folder structure before the components folder can be populated
         <>
             <h1>Void</h1>
         </>
