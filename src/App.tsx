53838c3ef294e3d30c64d5cca075f5e17ca8b427
Mauro <m.henriquesilva18@gmail.com>
2023-04-27 20:38:41 +0200

    feat: add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 export default function App() {
     return (
         <>
             <BrowserRouter>
+                <Navbar />
                 <Routes>
                     <Route path='/' element={<Home />} />
                 </Routes>
