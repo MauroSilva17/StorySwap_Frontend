aab942170e2a8f48f8b2fffc9c85b5522e40d06a
Mauro <m.henriquesilva18@gmail.com>
2023-04-27 20:39:30 +0200

    feat: add Mint and checkBalance button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,42 @@
-import React, { useState } from 'react'
-import { SearchOutlined } from '@ant-design/icons'
+import React, { useEffect, useState } from 'react'
+import { BrowserProvider, ethers } from 'ethers'
 import { Button, Col, Divider, Input, Row, Space } from 'antd'
+import { alchemyProvider, signer } from './../utils/provider'
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from './../utils/constants'
 
 const style: React.CSSProperties = { background: '#0092ff', padding: '8px 0' }
+
+// Random string sent to populate NFT
+const randomString = () => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcd23456789'
+    let result = ''
+    const length = 10
+    const values = crypto.getRandomValues(new Uint32Array(length))
+    values.forEach((value) => (result += chars[value % chars.length]))
+    const converted = BigInt(result)
+    return result
+}
 
 export default () => {
     const [balance, setBalance] = useState('0')
 
-    const checkBalance = () => {
-        setBalance('5')
+    const checkBalance = async () => {
+        // creating instance of contract
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, await signer)
+        // Get user user address
+        const userAdress = await signer.address
+
+        const userBalance = await contract.balanceOf(userAdress)
+        setBalance(userBalance)
+    }
+    const minter = async () => {
+        // creating instance of contract
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, await signer)
+        const userAddress = await signer.address
+
+        const randomURI = randomString()
+        const tx = await contract.safeMint(userAddress, randomURI)
+        await tx.wait()
     }
 
     return (
@@ -19,7 +47,7 @@ export default () => {
                 <Col className='gutter-row' span={4}></Col>
                 <Col className='center-col' span={16}>
                     <Row>
-                        <Button size='large' type='primary' danger ghost={true}>
+                        <Button size='large' type='primary' danger ghost={true} onClick={minter}>
                             Mint
                         </Button>
                     </Row>
