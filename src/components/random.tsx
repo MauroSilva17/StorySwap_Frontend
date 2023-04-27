f3488dc908d602cbd290dcf1113bf0f44693e05d
Mauro <m.henriquesilva18@gmail.com>
2023-04-27 20:35:31 +0200

    refactor: delete unused file

--- src/components/random.tsx
+++
@@ -1,8 +0,0 @@
-export default () => {
-    return (
-        //Empty component. Meant to create a folder structure before the components folder can be populated
-        <>
-            <h1>Void</h1>
-        </>
-    )
-}
