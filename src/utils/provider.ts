68f509d54704b88f108341a5557ff2694009d0ad
Mauro <m.henriquesilva18@gmail.com>
2023-05-07 21:44:38 +0200

    refactor: remove provider from front-end

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,7 +1,7 @@
-import { ethers } from 'ethers'
-import { API_KEY, PRIVATE_KEY } from './constants'
+// import { ethers } from 'ethers'
+// import { API_KEY, PRIVATE_KEY } from './constants'
 
-// Alchemy provider
-export const alchemyProvider = new ethers.AlchemyProvider('goerli', API_KEY)
-// Signer
-export const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider)
+// // Alchemy provider
+// export const alchemyProvider = new ethers.AlchemyProvider('goerli', API_KEY)
+// // Signer
+// export const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider)
