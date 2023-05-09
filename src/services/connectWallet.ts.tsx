159a7865a2c6a56c8a971c165e938c9d2a3ed618
Mauro <m.henriquesilva18@gmail.com>
2023-05-09 21:27:24 +0200

    fix: fix metamask request

--- src/services/connectWallet.ts.tsx
+++ src/services/connectWallet.ts.tsx
@@ -4,8 +4,7 @@ export const connectWallet = async () => {
     const provider = new ethers.BrowserProvider(window.ethereum)
 
     // MetaMask requires requesting permission to connect users accounts
-    await provider.send('eth_requestAccounts', [])
-
+    const account = await provider.send('eth_requestAccounts', [])
     // The MetaMask plugin also allows signing transactions to
     // send ether and pay to change state within the blockchain.
     // For this, you need the account signer...
